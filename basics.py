0ed9269cd5f8da4c6e64dfb537716433456fa03f
Kibet Cheruiyot <137798428+kibetbrian74@users.noreply.github.com>
2024-08-23 19:28:37 +0300

    Changed the y-axis in the numpy interpolation to be a range

--- basics.py
+++ basics.py
@@ -4,7 +4,7 @@ def get_distance(landmarks_list):
         return
     (x1, y1), (x2, y2) = landmarks_list[0], landmarks_list[1]
     L = np.hypot(x2 - x1, y2 - y1)
-    return np.interp(L, [0, 1], [0:1000])
+    return np.interp(L, [0, 1], [0, 1000])
 
 def get_angle(a, b, c):
     radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
